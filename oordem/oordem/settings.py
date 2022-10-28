ce986b126e0ab762fa4dbaf7032a651359927266
Giovanni <106890825+giovannins@users.noreply.github.com>
2022-10-28 11:38:55 -0300

    Update settings.py

--- oordem/oordem/settings.py
+++ oordem/oordem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i94b^kaybjw7&t(6e9o-@uhp1)o1sdq9w2j*==bua)kb&*wf7$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
